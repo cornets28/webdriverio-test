20a7469ec33c982bea387c3e87540407532d0bf5
CornetS28 <corsam28@gmail.com>
2020-02-05 15:41:30 -0500

    add gitinore file

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,6 +1,14 @@
 describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
+  const baseUrl = "http://www.webdriveruniversity.com/";
+  // let baseUrl;
+  // if (process.env.SERVER === "prod") {
+  //   baseUrl = "http://www.webdriveruniversity.com/";
+  // } else {
+  //   baseUrl = "http:localhost:3000";
+  // }
+
   it("check that the contact us button opens the contact us page", function(done) {
-    browser.url("http://www.webdriveruniversity.com/");
+    browser.url(baseUrl);
     let title = browser.getTitle();
     console.log("Title is: " + title);
     const contactButton = $("#contact-us");
@@ -9,7 +17,7 @@ describe("Verify whether webdriveruniversity links on homepage work correctly", 
   });
 
   it("check that the login button opens the login portal page", function(done) {
-    browser.url("http://www.webdriveruniversity.com/");
+    browser.url(baseUrl);
     const loginButton = $("#login-portal");
     loginButton.click();
     let title = browser.getTitle();
