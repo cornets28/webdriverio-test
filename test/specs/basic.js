8fd71b6aa35eec8bda59cee7bc4f8ac0419a637b
CornetS28 <corsam28@gmail.com>
2020-02-17 15:12:21 -0500

    check for url and title match

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -14,17 +14,27 @@ describe("Verify whether webdriveruniversity links on homepage work correctly", 
     expect(title).to.equal("WebDriverUniversity.com");
     // title.should.equal("WebDriverUniversity.com"); //or
     const contactButton = $("#contact-us");
+
     contactButton.click();
     browser.pause(3000);
+    browser.newWindow(baseUrl + "Contact-Us/contactus.html");
+    expect(browser.getUrl()).to.include("Contact-Us", "URL Mismatch");
+
+    expect(browser.getUrl()).to.equal(
+      "http://www.webdriveruniversity.com/Contact-Us/contactus.html"
+    );
+
+    expect(browser.getTitle()).to.include("WebDriver | Contact Us");
   });
 
   it("check that the login button opens the login portal page", function(done) {
     browser.url(baseUrl);
-    const loginButton = $("#login-portal");
-    loginButton.click();
+
     let title = browser.getTitle();
     assert.equal(title, "WebDriverUniversity.com");
     console.log("Title is: " + title);
+    const loginButton = $("#login-portal");
+    loginButton.click();
     browser.pause(3000);
   });
 });
