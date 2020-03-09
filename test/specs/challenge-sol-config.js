b1de475858ebc09056ebf313de5bb5b4b8f242c1
CornetS28 <corsam28@gmail.com>
2020-03-09 14:41:31 -0400

    advance addCommand, not yet works

--- test/specs/challenge-sol-config.js
+++ test/specs/challenge-sol-config.js
@@ -11,27 +11,55 @@ describe("Test contact us page on webdriveruni", () => {
         title: browser.getTitle()
       };
     });
+
+    browser.addCommand("waitAndClick", function(selector) {
+      try {
+        $(selector).waitForExist();
+        $(selector).click();
+      } catch (Error) {
+        throw new Error("Could not click on selector: " + $(selector));
+      }
+    });
+
+    browser.addCommand("waitAndSendkeys", function(selector, keys) {
+      try {
+        $(selector).waitForExist();
+        $(selector).setValue(keys);
+      } catch (Error) {
+        throw new Error(
+          "Could not send keys: " + $(keys) + ", using selector: " + $(selector)
+        );
+      }
+    });
+
     const pageDetails = browser.getUrlAndTitle();
     expect(pageDetails.url).to.contain("webdriveruniversity");
     expect(pageDetails.title).to.contain("WebDriverUniversity");
   });
 
   it("Submit all information via the contact us page", () => {
-    const contactUsButton = $('//h1[text()="CONTACT US"]/..');
-    contactUsButton.click();
+    // const contactUsButton = $('//h1[text()="CONTACT US"]/..');
+    //  contactUsButton.click();
+    browser.waitAndClick('//h1[text()="CONTACT US"]/..');
 
     browser.switchWindow("WebDriver | Contact Us");
-    const firstName = $('//*[@name="first_name"]');
-    const lastName = $('//*[@name="last_name"]');
-    const emailAddress = $('//*[@name="email"]');
-    const message = $('//*[@name="message"]');
-    const submitButton = $('//*[@value="SUBMIT"]');
+    // const firstName = $('//*[@name="first_name"]');
+    // const lastName = $('//*[@name="last_name"]');
+    // const emailAddress = $('//*[@name="email"]');
+    // const message = $('//*[@name="message"]');
+    browser.waitAndSendKeys('//*[@name="first_name"]', config.firstName);
+    browser.waitAndSendKeys('//*[@name="last_name"]', config.lastName);
+    browser.waitAndSendKeys('//*[@name="email"]', "joe_Blogs121@mail.com");
+    browser.waitAndSendKeys('//*[@name="message"]', "Hello how are you?");
+    browser.waitAndClick('//*[@value="SUBMIT"]');
 
-    firstName.setValue(config.firstName);
-    lastName.setValue(config.lastName);
-    emailAddress.setValue("joe_Blogs121@mail.com");
-    message.setValue("Hello how are you?");
-    submitButton.click();
+    // const submitButton = $('//*[@value="SUBMIT"]');
+
+    // firstName.setValue(config.firstName);
+    // lastName.setValue(config.lastName);
+    // emailAddress.setValue("joe_Blogs121@mail.com");
+    // message.setValue("Hello how are you?");
+    // submitButton.click();
 
     const contactUsSubmissionDetails = browser.getUrlAndTitle();
     browser.pause(6000);
